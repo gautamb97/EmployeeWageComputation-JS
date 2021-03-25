8e8932234bbd6cc11315642d9f7b277ba27ee506
gautamb97 <www.gautam971997@gmail.com>
2021-03-25 08:09:27 +0530

    Refactored:Stored the daily wage along with the total wage

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -6,7 +6,8 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
 const MAX_HRS_IN_MONTH = 160;
-
+let empDailyWageArr = new Array();
+let empHrs;
 function getWorkingHours(empCheck){
     switch (empCheck){
         case IS_PART_TIME:
@@ -18,13 +19,18 @@ function getWorkingHours(empCheck){
     }
 }
 
+function calculatingDailyWage(){
+    return empHrs * WAGE_PER_HOUR;
+}
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calculatingDailyWage(empHrs));
 }
 
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+let empWage = calculatingDailyWage(totalEmpHrs);
 console.log("Total Days: "+ totalWorkingDays +", Total Employee Hours: "+ totalEmpHrs +", Total Employee Wage : " + empWage);
